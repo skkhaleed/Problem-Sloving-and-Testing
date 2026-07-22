13d6763ff9c17995cdc2d8c2c11f688ff9f848de
skkhaleed <khaleed0861@gmail.com>
2026-07-22 04:57:24 +0000

    Initial commit

---
+++ task6.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class task6 {
+    public static void printPairs(int[] arr) {
+        int n = arr.length;
+        System.out.println("Printing all unique unordered pairs:");
+        
+        // Inner loop starts at i + 1 to avoid duplicate combinations
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                System.out.println("(" + arr[i] + ", " + arr[j] + ")");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        printPairs(arr);
+    }
+}
