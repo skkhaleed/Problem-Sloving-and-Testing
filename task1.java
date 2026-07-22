9e574e885dd2dfa22ecad28aa8672636757d067e
skkhaleed <khaleed0861@gmail.com>
2026-07-22 04:09:52 +0000

    Initial commit

---
+++ task1.java
@@ -0,0 +1,10 @@
+public class task1 {
+    public static void main(String[] args) {
+        // Declare and initialize the array
+        int[] arr = {10, 20, 30, 40, 50};
+        int targetIndex = 2;
+
+        // Access and print the element
+        System.out.println(arr[targetIndex]); // Output: 30
+    }
+}
